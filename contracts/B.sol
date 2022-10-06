0e72757e0d94219f4f554ce8fba4813678537805
fps <anthony.nnaemeka.umeh@gmail.com>
2022-10-06 15:00:04 +0100

    Creation

--- contracts/B.sol
+++ contracts/B.sol
@@ -7,7 +7,6 @@ import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 * @title Token B.
 * @author Anthony (fps) https://github.com/0xfps.
 * @dev Token B for Uniswap.
-* Pull testing.
 */
 contract B is ERC20 {
     constructor() ERC20("B", "BTK") {
