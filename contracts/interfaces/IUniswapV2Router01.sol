0062f3b4595351be77f3b00f58eb9eae21466187
Remix Workspace <>
2022-10-07 11:20:11 +0100

    Added necessary files.

--- contracts/interfaces/IUniswapV2Router01.sol
+++ contracts/interfaces/IUniswapV2Router01.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-3.0
-pragma solidity ^0.8.14;
+pragma solidity ^0.8.7;
 
 interface IUniswapV2Router01 {
     function factory() external pure returns (address);
--- contracts/interfaces/IUniswapV2Router02.sol
+++ contracts/interfaces/IUniswapV2Router02.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-3.0
-pragma solidity ^0.8.14;
+pragma solidity ^0.8.7;
 
 import './IUniswapV2Router01.sol';
 
